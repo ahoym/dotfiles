76e5a2022aa8901753b877b2dc57fed89f0653a7
ahoym <colmahoy@gmail.com>
2021-05-10 01:27:21 -0400

    Prefix with [include-*] sigils for searchability

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ BASIC_EMPTY_PROGRESS="          "
 GITCONFIG_PATH="$(pwd)/.gitconfig"
 # Extends the base .gitconfig file with the dotfiles .gitconfig
 if ! grep -q $GITCONFIG_PATH ~/.gitconfig; then
-echo "Adding [include] path for $(pwd)/.gitconfig to ~/.gitconfig"
+echo "[include-git] Adding [include] path for $(pwd)/.gitconfig to ~/.gitconfig"
 cat << EOT >> ~/.gitconfig
 
 [include]
@@ -15,7 +15,7 @@ cat << EOT >> ~/.gitconfig
 
 EOT
 else
-  echo "ahoym/dotfiles already [include]d in ~/.gitconfig. Skipping [include-git]."
+  echo "[nclude-git] ahoym/dotfiles already [include]d in ~/.gitconfig. Skipping [include-git]."
 fi
 echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS[20%]\r"
 
@@ -23,10 +23,10 @@ echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_
 VIM_COPY_SIGIL="File copied from ahoym/vim_related"
 # Copies vim related files/dirs if not existing
 if [ ! -f ~/.vimrc ] || ! grep -q "$VIM_COPY_SIGIL" ~/.vimrc; then
-  echo "Copying vim related files/dirs to root"
+  echo "[include-vim] Copying vim related files/dirs to root"
   cp -R ./vim_related/ ~
 else
-  echo "ahoym/vim_related already exists in ~/.vimrc. Skipping [include-vim]."
+  echo "[include-vim] ahoym/vim_related already exists in ~/.vimrc. Skipping [include-vim]."
 fi
 echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS[40%]\r"
 
