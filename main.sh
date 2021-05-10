3d92d5147d94e5485330bdd43c95817aecd7e5fe
ahoym <colmahoy@gmail.com>
2021-05-10 01:22:25 -0400

    Add scripts to install brew, nvm. Add progress %

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+BASIC_PROGRESS_SIGIL=">>>>>>>>>>"
+BASIC_EMPTY_PROGRESS="          "
+
 # [include-git]
 GITCONFIG_PATH="$(pwd)/.gitconfig"
 # Extends the base .gitconfig file with the dotfiles .gitconfig
@@ -14,6 +17,7 @@ EOT
 else
   echo "ahoym/dotfiles already [include]d in ~/.gitconfig. Skipping [include-git]."
 fi
+echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS[20%]\r"
 
 # [include-vim]
 VIM_COPY_SIGIL="File copied from ahoym/vim_related"
@@ -24,4 +28,32 @@ if [ ! -f ~/.vimrc ] || ! grep -q "$VIM_COPY_SIGIL" ~/.vimrc; then
 else
   echo "ahoym/vim_related already exists in ~/.vimrc. Skipping [include-vim]."
 fi
+echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS[40%]\r"
 
+# [include-brew]
+# Check for homebrew, install if it doesn't exist. Update if it does
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "[include-brew] Homebrew not found, installing."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "[include-brew] Homebrew exists, updating."
+  brew update
+fi
+echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS$BASIC_EMPTY_PROGRESS[60%]\r"
+
+# [include-nvm]
+# Check for nvm, install if it doesn't exist.
+if [ ! -d ~/.nvm ] ; then
+  echo "[include-nvm] nvm not found, installing"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+else
+  echo "[include-nvm] nvm found, installing stable and setting as default"
+  . ~/.nvm/nvm.sh
+  nvm install stable
+  nvm alias default stable
+fi
+echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_EMPTY_PROGRESS[80%]\r"
+
+
+echo -ne "$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL$BASIC_PROGRESS_SIGIL[100%]\r"
